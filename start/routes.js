b3b97b3a205e21fe0de372591918a54177b25f72
Roberson <roberson.cardoso@codus.com.br>
2019-07-02 15:15:43 -0300

    Adds project index and create

--- start/routes.js
+++ start/routes.js
@@ -38,5 +38,9 @@ Route.group(() => {
 Route.group(() => {
   Route.post('auth/register', 'UserController.register');
   Route.post('auth/login', 'UserController.login'); 
+
+
+  Route.get('projects', 'ProjectController.index').middleware('auth');
+  Route.post('projects', 'ProjectController.create').middleware('auth');
 })
  .prefix('api/v1');
