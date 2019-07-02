f906d86b3be548aa399e56878589bcbac8f4399b
Roberson <roberson.cardoso@codus.com.br>
2019-07-02 13:09:31 -0300

    Adding routes to uses UserController methods

---
+++ start/routes.js
@@ -0,0 +1,42 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.get('/', () => {
+  return { greeting: 'Hello world in JSON' }
+})
+
+
+Route.group(() => {
+  Route.post('auth/register', 
+    ({request}) => {
+      return {message: 'hello arrow function!',}
+    }
+  );
+})
+ .prefix('api/v0');
+/* = Route.post('api/auth/register', ({request} => {
+      return { message: 'hello arrow function!', }
+    })
+  );*/ 
+
+
+Route.group(() => {
+  Route.post('auth/register', 'UserController.register');
+  Route.post('auth/login', 'UserController.login'); 
+})
+ .prefix('api/v1');
